491b55394baa69fe06bb1f442106f30e1e30c2d5
Shtolle <eva.semenov1@gmail.com>
2022-11-27 07:09:02 +0000

    push

--- FrontEnd/src/screens/ProjectsScreen.js
+++ FrontEnd/src/screens/ProjectsScreen.js
@@ -1,7 +1,12 @@
 import ProjectCardScroll from "../components/projectCardScroll.js";
+import fonts from "../constants/fonts.js";
+import { useContext } from "react";
+import { NavContext } from "../context.js";
+import colors from "../constants/colors.js";
 // import Picker from "../components/Picker.js";
 
 export default function ProjectsScreen() {
+  const { setScreenName } = useContext(NavContext);
   return (
     <div
       style={{
@@ -14,8 +19,35 @@ export default function ProjectsScreen() {
         width: "100%",
       }}
     >
-      {ProjectCardScroll({ title: "Live projects", subtitle: "Subtitle", description: "No description" })}
+      {/* {ProjectCardScroll({ title: "Live projects", subtitle: "Subtitle", description: "No description" })}
+       */}
+      {/* <div>
+        <text style={{ fontSize: 45, fontWeight: fonts.Regular }}>
+          You don’t have any live
+          <br />
+          contracts at the moment.
+        </text>
+        <button style={styles.mainBtn} onClick={() => setScreenName("Create")}>
+          <p style={styles.btnText}>Create a contract</p>
+        </button>
+      </div> */}
       {ProjectCardScroll({ title: "Previous projects", subtitle: "Subtitle", description: "No description" })}
     </div>
   );
 }
+const styles = {
+  mainBtn: {
+    padding: 15,
+
+    paddingLeft: 70,
+    paddingRight: 70,
+    borderRadius: 10,
+    border: 0,
+    backgroundColor: colors.main,
+    marginTop: 70,
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "center",
+  },
+  btnText: { color: colors.white, alignItems: "center", justifyContent: "center", paddingTop: 10, fontWeight: fonts.Bold, fontSize: 26 },
+};
